18090181ac1ab74b00f9b5dd872a5d5cc8e5baf6
QuocCuong <quoc.cuong.yb.dhtb@gmail.com>
2024-09-28 22:54:38 +0700

    Initial

---
+++ src/main/java/com/cuongpq/seed/SeedApplication.java
@@ -0,0 +1,14 @@
+package com.cuongpq.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedApplication.class, args);
+    }
+}
